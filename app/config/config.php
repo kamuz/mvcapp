b2c459da282816e3cee52ecf1a914f60287eef43
Vladimir Kamuz <v.kamuz@gmail.com>
2018-05-01 07:55:29 +0300

    Config file and autoload libraries

---
+++ app/config/config.php
@@ -0,0 +1,8 @@
+<?php
+
+// App Root
+define('APPROOT', dirname(dirname(__FILE__)));
+// URL Root
+define('URLROOT', 'http://mvcapp.loc');
+// Site Name
+define('SITENAME', 'Simple MVC');
--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,1 +1,5 @@
 <h1><?php echo $data['title'] ?></h1>
+
+<?php echo APPROOT . '<br>'; ?>
+<?php echo URLROOT . '<br>'; ?>
+<?php echo SITENAME . '<br>'; ?>
